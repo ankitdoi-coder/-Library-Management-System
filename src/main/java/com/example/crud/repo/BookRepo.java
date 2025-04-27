f9b9904e43d345178220a9b573709d15df87c029
ankitdoi--coder <ankitdoi82@gmail.com>
2025-04-27 21:36:54 +0530

    new commit

---
+++ src/main/java/com/example/crud/repo/BookRepo.java
@@ -0,0 +1,12 @@
+package com.example.crud.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.crud.model.Book;
+@Repository
+
+public interface BookRepo extends JpaRepository<Book, Long> 
+{
+   
+}
